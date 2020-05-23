91644fec9043f59337286c75cda97991f88da7e7
Marcos R Oliveira <mroger.oliveira@gmail.com>
2020-05-23 18:03:46 -0300

    First commit

--- src/main/java/br/org/roger/estudos/threadscheduler/configuration/ThreadschedulerConfiguration.java
+++ src/main/java/br/org/roger/estudos/threadscheduler/configuration/ThreadschedulerConfiguration.java
@@ -10,7 +10,7 @@ public class ThreadschedulerConfiguration {
     @Bean
     public ThreadPoolTaskScheduler myScheduler() {
         final ThreadPoolTaskScheduler tpts = new ThreadPoolTaskScheduler();
-        tpts.setThreadNamePrefix("TNP");
+        tpts.setThreadNamePrefix("TNP-");
         tpts.setPoolSize(5);
         tpts.setAwaitTerminationSeconds(5);
         tpts.setWaitForTasksToCompleteOnShutdown(true);
